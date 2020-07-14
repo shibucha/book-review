8a20529e5e8f261a503a37a1cd9626fa73dae2fd
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-07-14 15:56:18 +0900

    共通ページの作成

---
+++ book-review/resources/views/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>
+    @yield('title')
+  </title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- My custom styles -->
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+<div id="app">
+    @yield('content')
+</div>
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- My custom styles -->
+  <script src="{{ asset('/js/app.js') }}"></script>
+</body>
+
+</html>
