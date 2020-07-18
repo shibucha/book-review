07516eb44e6c9690afcd3590aeac8f082eb37243
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-07-18 21:34:07 +0900

    ログインページの修正

--- book-review/resources/views/auth/login.blade.php
+++ book-review/resources/views/auth/login.blade.php
@@ -3,5 +3,42 @@
 @section('title', 'ログイン')
 
 @section('content')
-<p>ログイン画面です</p>
+<div class="container">
+    <div class="row">
+        <div class="card">
+
+            <h5 class="card-header info-color white-text text-center py-4">
+                <strong>ログイン</strong>
+            </h5>
+
+            <!--Card content-->
+            <div class="card-body px-lg-5 pt-0">
+
+                <!-- Form -->
+                <form class="text-center" style="color: #757575;" method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <!-- メールアドレス -->
+                    <div class="md-form mt-0">
+                        <input type="email" id="email" name="email" class="form-control" required value="{{ old('email') }}">
+                        <label for="email">メールアドレス</label>
+                    </div>
+
+                    <!-- パスワード -->
+                    <div class="md-form">
+                        <input type="password" id="password"" name=" password" class="form-control" required>
+                        <label for="password">パスワード</label>
+                    </div>
+
+                    <!-- 登録ボタン -->
+                    <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">ログイン</button>
+
+                </form>
+
+                <div class="mt-0">
+                    <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
