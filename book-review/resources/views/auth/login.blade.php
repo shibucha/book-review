f697e72cfbb5fcd8b4a220e24737510791eb4798
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-07-19 16:35:51 +0900

    ログインページの修正（route,リメンバートークン）

--- book-review/resources/views/auth/login.blade.php
+++ book-review/resources/views/auth/login.blade.php
@@ -1,8 +1,9 @@
-@extends('app')
+@extends('layouts.app')
 
 @section('title', 'ログイン')
 
 @section('content')
+@include('layouts.nav')
 <div class="container">
     <div class="row">
         <div class="card">
@@ -15,19 +16,22 @@
             <div class="card-body px-lg-5 pt-0">
 
                 <!-- Form -->
-                <form class="text-center" style="color: #757575;" method="POST" action="{{ route('register') }}">
+                <form class="text-center" style="color: #757575;" method="POST" action="{{ route('login') }}">
                     @csrf
                     <!-- メールアドレス -->
                     <div class="md-form mt-0">
-                        <input type="email" id="email" name="email" class="form-control" required value="{{ old('email') }}">
+                        <input type="text"" id="email" name="email" class="form-control" required value="{{ old('email') }}">
                         <label for="email">メールアドレス</label>
                     </div>
 
                     <!-- パスワード -->
                     <div class="md-form">
-                        <input type="password" id="password"" name=" password" class="form-control" required>
+                        <input type="password" id="password"" name="password" class="form-control" required>
                         <label for="password">パスワード</label>
                     </div>
+
+                    <!-- リメンバートークン -->
+                    <input type="hidden" nama="remember" id="remember" value="on">
 
                     <!-- 登録ボタン -->
                     <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">ログイン</button>
