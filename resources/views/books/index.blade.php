e46b6fb4ed6bd40196a7704fce195983ca94f651
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-08-07 14:48:38 +0900

    マイページの追記

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -11,16 +11,20 @@
 </div>
 @endif
 
-<p>
-    {{ $user->name }}{{ $user->id }}
-</p>
-
-<a href="{{route('books.profile',['user_id'=>$user->id])}}">
-    @if($user->icon)
-    <img src="/storage/icons/{{ $user->icon }}" alt="プロフィール画像" width="200px" width="200px">
-    @else
-    <img src="/storage/icons/default.png" alt="プロフィール画像" width="200px" width="200px">
-    @endif
-</a>
+<div class="user">
+    <div class="user__info">
+        <div class="user__name"><i class="fas fa-user-circle"></i>{{ $user->name}}さんのマイページ</div>
+        <div class="user__bookCount"><i class="fas fa-book-open"></i>{{ $user->name}}さんがこれまでに読んだ冊数 : ○○冊</div>
+    </div>
+    <figure class="user__image">
+        <a href="{{route('books.profile',['user_id'=>$user->id])}}">
+            @if($user->icon)
+            <img src="/storage/icons/{{ $user->icon }}" alt="プロフィール画像" width="200px" width="200px">
+            @else
+            <img src="/storage/icons/default.png" alt="プロフィール画像" width="200px" width="200px">
+            @endif
+        </a>
+    </figure>
+</div>
 
 @endsection
