5a8c97dcf48027456f7e50a948a8e310bb085490
かいっと <68046289+kaitto-0@users.noreply.github.com>
2020-08-19 20:58:08 +0900

    ログイン画面と新規登録画面の調整

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,47 +4,42 @@
 
 @section('content')
 @include('layouts.nav')
-<div class="container">
-    <div class="row">
-        <div class="card">
 
-            <h5 class="card-header info-color white-text text-center py-4">
-                <strong>ログイン</strong>
-            </h5>
+<div class="for-backgrounds">
+    <div class="login-wrapper">
+      <h1>ログイン</h1>
+        <div class="login-container">
 
             @include('layouts.error_list')
-            
-            <!--Card content-->
-            <div class="card-body px-lg-5 pt-0">
 
-                <!-- Form -->
-                <form class="text-center" style="color: #757575;" method="POST" action="{{ route('login') }}">
-                    @csrf
-                    <!-- メールアドレス -->
-                    <div class="md-form mt-0">
-                        <input type="text"" id=" email" name="email" class="form-control" required value="{{ old('email') }}">
-                        <label for="email">メールアドレス</label>
-                    </div>
-
-                    <!-- パスワード -->
-                    <div class="md-form">
-                        <input type="password" id="password"" name=" password" class="form-control" required>
-                        <label for="password">パスワード</label>
-                    </div>
-
-                    <!-- リメンバートークン -->
-                    <input type="hidden" nama="remember" id="remember" value="on">
-
-                    <!-- 登録ボタン -->
-                    <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">ログイン</button>
-
-                </form>
-
-                <div class="mt-0">
-                    <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
+            <!-- FORM -->
+            <form action="{{ route('login') }}" method="post"></form>
+                @csrf
+            <!-- ユーザー名 -->
+                <div class="entry-field">
+                    <input type="text" id="email" name="email" class="form-control" required value="{{ old('email') }}" placeholder="ユーザー名を入力">
                 </div>
+
+            <!-- パスワード -->
+                <div class="entry-field password-field">
+                    <input type="password" id="password" name="password" class="form-control" required placeholder="パスワードを入力">
+                </div>
+
+            <!-- リメンバートークン -->
+                <input type="hidden" nama="remember" id="remember" value="on">
+
+            <!-- 登録ボタン -->
+                <button class="btn btn-rounded btn-block my-4 waves-effect z-depth-0 login-button" type="submit">ログイン</button>
+
+            </form>
+
+            <p class="no-account">アカウントをお持ちでない方はこちら</p>
+
+            <div class="to-register-wrapper">
+                <a class="btn btn-rounded btn-block waves-effect z-depth-0 register-button" href="{{ route('register')}}" role="button">新規登録</a>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
