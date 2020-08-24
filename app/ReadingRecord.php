293459bab73283f516b5705d7666408e894516c7
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-08-24 18:09:46 +0900

    の追記

--- app/ReadingRecord.php
+++ app/ReadingRecord.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReadingRecord extends Model
-{       
+{      
+    protected $fillable =[
+        'reading_date',
+        'body',
+    ]; 
+    
     public function user() : BelongsTo
     {
         return $this->belongsTo('App\User');
