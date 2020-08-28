5ab89aacda63724f95abdecfedf8375e345f94e6
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-08-28 12:05:58 +0900

    レビューページの編集

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -16,7 +16,7 @@
 @if(array_key_exists('imageLinks', $item['volumeInfo']))
 <img src="{{ $item['volumeInfo']['imageLinks']['thumbnail'] }}" alt="書籍のイメージ"><br>
 @else
-<img src="/storage/icons/default.png" alt="プロフィール画像" width="200px" width="200px">
+<img src="/storage/book/book_noimage.png" alt="書籍のイメージ" width="200px" width="200px">
 @endif
 
 <!-- 書籍のタイトル -->
@@ -24,14 +24,17 @@
 <P>{{ $item['volumeInfo']['title']}}</P>
 @endif
 
+<!-- 著者 -->
 @if(array_key_exists('authors', $item['volumeInfo']))
 著者：{{ $item['volumeInfo']['authors'][0] }}/
 @endif
 
+<!-- 出版日 -->
 @if(array_key_exists('publishedDate', $item['volumeInfo']))
 出版日：{{ $item['volumeInfo']['publishedDate'] }}
 @endif
 
+<!-- 概要 -->
 @if(array_key_exists('description', $item['volumeInfo']))
 <p>概要<br>{{ $item['volumeInfo']['description'] }}</p>
 @endif
@@ -41,16 +44,20 @@
 <!----------------- END 書籍のAPI情報 --------------->
 
 <hr>
+
 <!----------------- START 自分の感想 --------------->
-<div>読んだ本の感想/編集</div>
+<div><i class="far fa-smile"></i>{{$review->user->name}}さんの感想</div>
 @if($review->body)
 <p>{{ $review->body }}</p>
 @else
 <p>まだ感想は登録されていません。</p>
 @endif
 <!----------------- END 自分の感想 --------------->
+
 <hr>
+
 <!----------------- START 他人の感想 --------------->
+<div><i class="far fa-smile"></i>みんなの感想</div>
 @if($others_reviews->count() > 0)
 @foreach($others_reviews as $other_review)
 <div>{{ $other_review->user->name }}さんの感想</div>
