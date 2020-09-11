6b3b7b038dad3d844db2e402ae557c5091b1c51e
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-09-11 20:48:50 +0900

    bladeのパーツ整理

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,71 @@
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light navbar-t" style="background-color: rgba(110, 80, 100, 0.1);">
+
+    <!-- Navbar brand -->
+    @guest
+    <a class="navbar-brand" href="{{ route('index') }}" ><img src="" alt="">Your Bookshelf</img></a>
+    @endguest
+    @auth
+    <a class="navbar-brand" href="{{ route('books.index') }}"><img src="" alt="">Your Bookshelf</img></a>
+    @endauth
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="basicExampleNav">
+
+        <!-- Links -->
+        <ul class="navbar-nav ml-auto">
+            <!-- <form class="form-inline">
+                <div class="md-form my-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="本を探す" aria-label="Search">
+                </div>
+            </form> -->
+ 
+            @guest
+            <li class="nav-item for-none">
+                <a class="btn btn-primary-s" href="{{ route('register')}}" role="button">新規登録</a>
+            </li>
+            <li class="nav-item for-r-li">
+                <a class="btn-primary-s for-r-sign" href="{{ route('register')}}">新規登録</a>
+            </li>    
+            @endguest
+
+            @guest
+            <li class="nav-item for-none">
+                <a class="btn btn-primary-l" href="{{ route('login')}}" role="button">ログイン</a>
+            </li>
+            <li class="nav-item for-r-li">
+                <a class="btn-primary-l for-r-sign" href="{{ route('login')}}" role="button">ログイン</a>
+            </li>
+            @endguest
+
+            @auth
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('books.search')}}"><i class="fas fa-book-open"></i>本を検索する</a>
+            </li>
+            @endauth
+
+            @auth
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">メニュー</a>
+                <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <button class="dropdown-item" onclick="location.href='{{ route('books.index') }}'">マイページ</button>
+                    <button type="submit" class="dropdown-item" form="logout-button">ログアウト</button>
+                </div>
+            </li>
+
+            <form id="logout-button" method="POST" action="{{ route('logout') }}">
+                @csrf
+            </form>
+            @endauth
+            <!-- Dropdown -->
+        </ul>
+    </div>
+    <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
---
+++ resources/views/includes/read-register.blade.php
@@ -0,0 +1,57 @@
+<!-- モーダル 本の登録 -->
+<div class="modal fade" id="{{ $item['id'] }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+
+    <!-- Add .modal-dialog-centered to .modal-dialog to vertically center the modal -->
+    <div class="modal-dialog modal-dialog-centered" role="document">
+
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title modal__title" id="exampleModalLongTitle">読んだ本に登録</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <!-- 書籍タイトル -->
+            <p class="modal__book-title">タイトル：{{ $item['volumeInfo']['title']}}</p>
+
+            <!-- 登録フォーム -->
+            <form action="{{ route('search.store',['book_id'=>$item['id']]) }}" method="POST">
+                @csrf
+                @include('layouts.error_list')
+
+                <div class="modal-body modal__body">
+
+                    <div class="modal__book-title modal__left">
+                        <div class="modal__book-image">
+                            @if(array_key_exists('imageLinks', $item['volumeInfo']))
+                            <img src="{{ $item['volumeInfo']['imageLinks']['thumbnail']}}" alt="書籍のイメージ"><br>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="modal__right">
+                        <div class="modal__date form-group">
+                            <label for="reading_record">読了日:</label>
+                            <input type="date" name="reading_date" class="reading_date modal__input-field modal__date-field">
+                        </div>
+                        <div class="form-group modal__review">
+                            <label for="">感想・レビュー:</label>
+                            <textarea name="body" placeholder="感想・レビュー" cols="50" rows="10" value="{{ old('body')}}" class="modal__input-field"></textarea>
+                        </div>
+                        <div class="modal__private">
+                            <input type="checkbox" name="public_private" value="0">非公開にする。
+                        </div>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
+                    <button type="submit" class="btn btn-primary">登録する。</button>
+                </div>
+            </form>
+            <!-- 登録フォーム -->
+        </div>
+    </div>
+</div>
