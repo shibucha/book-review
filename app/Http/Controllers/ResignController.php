b81f66e1a41c30634885eacc15307ee0ccb7a3ba
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-09-15 13:59:55 +0900

    ストレージからプロフィール画像を削除してから退会する。

--- app/Http/Controllers/ResignController.php
+++ app/Http/Controllers/ResignController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use Illuminate\Support\Facades\Storage;
 
 class ResignController extends Controller
 {
@@ -15,8 +16,13 @@ class ResignController extends Controller
 
     public function destroy(){
         $user_id = Auth::id();        
-        $user_resign = User::find($user_id);        
-       
+        $user_resign = User::find($user_id);
+
+        // プロフィール画像をストレージから削除。
+        if($user_resign->icon){
+            Storage::disk('local')->delete('public/icons/' . $user_resign->icon);
+        }
+
         $user_resign->delete();
 
         return redirect()->route('index');
