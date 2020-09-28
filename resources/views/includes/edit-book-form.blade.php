07506cf80e2e8eb5a45cb1b2942243c1e16c5c0d
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-09-28 09:53:42 +0900

    モーダルの見た目編集_01

--- resources/views/includes/edit-book-form.blade.php
+++ resources/views/includes/edit-book-form.blade.php
@@ -7,14 +7,14 @@
 
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title modal__title" id="exampleModalLongTitle">感想を編集する。</h5>
+                <h5 class="modal-title modal__title" id="exampleModalLongTitle">読んだ本を編集する。</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
 
             <!-- 書籍タイトル -->
-            <p class="modal__book-title">タイトル：{{ $review->book->title }}</p>
+            <p class="modal__book-title">{{ $review->book->title }}</p>
 
             <!-- 編集・登録フォーム -->
             <form action="{{ route('books.update',['reading_record_id'=>$review->id]) }}" method="POST">
@@ -43,39 +43,39 @@
                             <label for="">感想・レビュー:</label>
                             <textarea name="body" placeholder="感想・レビュー" cols="50" rows="10" class="modal__input-field">{{ $review->body ?? old('body')}}</textarea>
                         </div>
-                        <div class="modal__private">
-                            @if($review->public_private === 1)
-                            <input type="checkbox" name="public_private" value="0">非公開にする。
-                            @else
-                            <input type="checkbox" name="public_private" value="1">公開する。
-                            @endif
-                        </div>
-                        <div class="modal__netabare">
-                            @if($review->netabare === "on")
-                            <input type="checkbox" name="netabare" value="off">ネタバレ無し
-                            @else
-                            <input type="checkbox" name="netabare" value="on">ネタバレ有り
-                            @endif
+
+                        <div class="modal__netabare-private">
+                            <div class="modal__private">
+                                @if($review->public_private === 1)
+                                <input type="checkbox" name="public_private" value="0">非公開にする
+                                @else
+                                <input type="checkbox" name="public_private" value="1">公開する。
+                                @endif
+                            </div>
+                            <div class="modal__netabare">
+                                @if($review->netabare === "on")
+                                <input type="checkbox" name="netabare" value="off">ネタバレ無し
+                                @else
+                                <input type="checkbox" name="netabare" value="on">ネタバレ有り
+                                @endif
+                            </div>
                         </div>
 
-                        <div id="star">
+                        <!-- 評価設定 -->
+                        <div class="modal__star">
                             <!-- 選択した星の値をhiddenで送信する。「:value="rating"」で値を取得する -->
-                            <input type="radio" name="rating" :value="rating" />再評価する。
-                            <star-rating 
-                            v-model="rating" 
-                            @rating-selected="setRating" 
-                            v-bind:increment="0.5" 
-                            v-bind:star-size="20" 
-                            v-bind:rating="0">
-                            </star-rating>
-                            
-        <input type="radio" name="rating" value="{{ $review->rating }}" checked>再評価しない
+                            <div class="modal__star-rating">
+                                <input type="radio" name="rating" :value="rating" /><div>再評価</div>
+                                <star-rating v-model="rating" @rating-selected="setRating" v-bind:increment="0.5" v-bind:star-size="20" v-bind:rating="0"></star-rating>
+                            </div>
+                            <div class="modal__star-not-rating">
+                                <input type="radio" name="rating" value="{{ $review->rating }}" checked>再評価しない
+                            </div>
                         </div>
                     </div>
 
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
+                <div class="modal-footer">                    
                     <button type="submit" class="btn btn-primary">編集する。</button>
                 </div>
             </form>
