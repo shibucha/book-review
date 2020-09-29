8cdb20a21de0ae213e824b4c4aaa1a78696d6e7c
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-09-29 21:11:57 +0900

    マイページの見た目作成開始_01

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -4,69 +4,76 @@
 
 @section('content')
 
-
-@if(session('success'))
-<div class="alert alert-success">
-    {{session('success')}}
-</div>
-@endif
-
-<div class="user">
-    <div class="user__info">
-        <div class="user__name"><i class="fas fa-user-circle"></i> {{ $user->name}} さんのマイページ</div>
-        <div class="user__bookCount"><i class="fas fa-book-open"></i> 読書数 : {{ $number_of_readings }}冊</div>
-        <div class="user__reviewCount"><i class="fas fa-pencil-alt"></i> レビュー数 : {{ $reviews_count }}冊</div>
-    </div>
-    <figure class="user__image">
-        <a href="{{route('books.profile',['user_id'=>$user->id])}}">
-            @if($user->icon)
-            <img src="{{ $user->icon }}" alt="プロフィール画像" width="200px" width="200px">
-            @else
-            <img src="https://book-review-shibucha.s3.ap-northeast-1.amazonaws.com/icons/default.png" alt="プロフィール画像" width="200px" width="200px">
-            @endif
-        </a>
-    </figure>
-</div>
-
-<div class="search">
-    @include('includes.error_list')
-    <form action="{{ route('books.search') }}" class="search__form" method="GET">
-        <input type="text" name="keyword" placeholder="キーワードで検索。" style="border:1px solid black;">
-        <input type="submit" value="検索">
-    </form>
-</div>
-
-<!-- 登録したレビュー一覧 -->
-<div class="review">
-    @if($reviews_count > 0)
-    @foreach($reviews as $review)
-    <div class="reading-book">
-        @if($review->book->image)
-        <img src="{{$review->book->image}}" alt="登録した本のイメージ" class="reading-book_img">
-        @else
-        <img src="/storage.book/book_noimage.png" alt="画像はありません" class="reading-book_img">
+<div class="container">
+    <div class="mypage__inner">
+        @if(session('success'))
+        <div class="alert alert-success">
+            {{session('success')}}
+        </div>
         @endif
 
-        <div class="reading-book_title">
-            書籍名：{{ $review->book->title }}｜
-            <!-- レビューを登録しているかどうか<div class=""></div> -->
-            @if($review->body)
-            <i class="fas fa-pencil-alt"></i>レビュー済
-            @endif
+        <div class="user mypage__section">
+            <div class="user__inner">
+                <figure class="user__image">
+                    <div class="user__name"><i class="fas fa-user-circle"></i> {{ $user->name}} さんのマイページ</div>
+                    <a href="{{route('books.profile',['user_id'=>$user->id])}}">
+                        @if($user->icon)
+                        <img src="{{ $user->icon }}" alt="プロフィール画像" width="200px" width="200px">
+                        @else
+                        <img src="https://book-review-shibucha.s3.ap-northeast-1.amazonaws.com/icons/default.png" alt="プロフィール画像" width="200px" width="200px">
+                        @endif
+                    </a>
+                </figure>
+                <div class="user__info">
+                    <div class="user__bookCount"><i class="fas fa-book-open"></i> 読書数 : {{ $number_of_readings }}冊</div>
+                    <div class="user__reviewCount"><i class="fas fa-pencil-alt"></i> レビュー数 : {{ $reviews_count }}冊</div>
+                </div>
+            </div>
         </div>
 
-        <div class="reading-book_date">読了日：{{ $review->reading_date }}</div>
-        <a href="{{ route('books.show', ['book_id' => $review->book->book_id])}}">
-            <button>この本について</button>
-        </a>
+        <div class="search mypage__section">
+            @include('includes.error_list')
+            <form action="{{ route('books.search') }}" class="search__form" method="GET">
+                <div class="search__box">
+                    <input class="search__field" type="text" name="keyword" placeholder="キーワードで検索。">
+                    <button class="search_btn" type="submit"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+
+        <!-- 登録したレビュー一覧 -->
+        <div class="review mypage__section">
+            @if($reviews_count > 0)
+            @foreach($reviews as $review)
+            <div class="review__book">
+                @if($review->book->image)
+                <img src="{{$review->book->image}}" alt="登録した本のイメージ" class="reading-book_img">
+                @else
+                <img src="/storage.book/book_noimage.png" alt="画像はありません" class="reading-book_img">
+                @endif
+
+                <div class="review__title">
+                    書籍名：{{ $review->book->title }}｜
+                    <!-- レビューを登録しているかどうか<div class=""></div> -->
+                    @if($review->body)
+                    <i class="fas fa-pencil-alt"></i>レビュー済
+                    @endif
+                </div>
+
+                <div class="review__date">読了日：{{ $review->reading_date }}</div>
+                <a href="{{ route('books.show', ['book_id' => $review->book->book_id])}}">
+                    <button>この本について</button>
+                </a>
+            </div>
+            @endforeach
+
+            <!--  ペジネーション機能 -->
+            {{$reviews->links()}}
+
+            @else
+            <p>まだ本は登録されていません。</p>
+            @endif
+        </div>
     </div>
-    @endforeach
-
-    <!--  ペジネーション機能 -->
-    {{$reviews->links()}}
-
-    @else
-    <p>まだ本は登録されていません。</p>
-    @endif
 </div>
 @endsection
