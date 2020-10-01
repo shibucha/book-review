3cb917eebe3a3e8a10083b2885089d0e386ce808
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-10-01 20:55:30 +0900

    プロフィール画像設定画面の見た目

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,59 +5,69 @@
 @section('content')
 
 <!-- プロフィール画像編集面 -->
-<div class="row">
-  @include('includes.error_list')
-  <form action="{{route('books.profile',['user_id'=>$user->id]) }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <div class="card card-body mb-3 col-12">
-      <p>プロフィール画像の編集</p>
-      <div class="media d-block d-md-flex">
+<div class="profile profile__container">
 
-        <!-- プロフィール画像 -->
-        <small>現在のプロフィール画像</small>
-        @if($user->icon)
-        <img src="{{ $user->icon }}" alt="プロフィール画像" width="200px" width="200px">
-        @else
-        <img src="https://book-review-shibucha.s3.ap-northeast-1.amazonaws.com/icons/default.png" alt="プロフィール画像" width="200px" width="200px">
-        @endif
+  <div class="profile__inner">
+    @include('includes.error_list')
+    <form class="profile__form" action="{{route('books.profile',['user_id'=>$user->id]) }}" method="POST" enctype="multipart/form-data">
+      @csrf
 
-        <div class="media-body text-center text-md-left ml-md-3 ml-0">
-          <input type="file" name="icon">
-          <button type="submit" class="btn btn-primary btn-md">画像を設定する</button>
+      <p class="profile__title">プロフィール画像の編集</p>
+
+      <div class="profile__block">
+        <div class="profile__icon">
+          <!-- プロフィール画像 -->
+          @if($user->icon)
+          <img class="profile__image" src="{{ $user->icon }}" alt="プロフィール画像">
+          @else
+          <img class="profile__image" src="https://book-review-shibucha.s3.ap-northeast-1.amazonaws.com/icons/default.png" alt="プロフィール画像">
+          @endif
+        </div>
+
+        <div class="profile__upload">
+          <label for="profile__input" class="profile__btn profile__btn-select">
+            画像を選ぶ。
+            <input type="file" name="icon" id="profile__input">
+          </label>
+
+          <!-- プロフィール画像の設定ボタン -->
+          <button type="submit" class="profile__btn profile__btn-set">画像を設定する</button>
 
           <!-- プロフィール画像の削除ボタン -->
-          <a class="text-danger" data-toggle="modal" data-target="#modal-delete-{{ $user->id }}">
-            <button class="btn btn-danger btn-md">画像を削除する</button>
+          <a class="" data-toggle="modal" data-target="#modal-delete-{{ $user->id }}">
+            <button class="profile__btn profile__btn-delete">画像を削除する</button>
           </a>
         </div>
       </div>
-    </div>
-  </form>
 
-  <!-- モーダル -->
-  <div id="modal-delete-{{ $user->id }}" class="modal fade" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-            <span aria-hidden="true">&times;</span>
-          </button>
+
+    </form>
+
+    <!-- モーダル -->
+    <div id="modal-delete-{{ $user->id }}" class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <form method="POST" action="{{ route('books.profile.destroy', ['user_id'=>$user->id])}}">
+            @csrf
+            @method('DELETE')
+            <div class="modal-body">
+              プロフィール画像を削除します。よろしいですか？
+            </div>
+            <div class="modal-footer justify-content-between">
+              <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+              <button type="submit" class="btn btn-danger">削除する</button>
+            </div>
+          </form>
         </div>
-        <form method="POST" action="{{ route('books.profile.destroy', ['user_id'=>$user->id])}}">
-          @csrf
-          @method('DELETE')
-          <div class="modal-body">
-            プロフィール画像を削除します。よろしいですか？
-          </div>
-          <div class="modal-footer justify-content-between">
-            <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-            <button type="submit" class="btn btn-danger">削除する</button>
-          </div>
-        </form>
       </div>
     </div>
+    <!-- モーダル -->
   </div>
-  <!-- モーダル -->
 
 </div>
 
