a4aa500e6f45b2773d264eafaf1d12aa6c39b568
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-10-02 19:48:07 +0900

    プロフィール画像更新時のプレビュー表示

--- resources/js/libs/Profile.js
+++ resources/js/libs/Profile.js
@@ -1,7 +1,8 @@
 export class Profile {
-    constructor(input, new_icon) {
+    constructor(input, new_icon, img_src) {
         this.inputFile = document.getElementById(input);
         this.newIconName = document.getElementById(new_icon);
+        this.profileImage = document.querySelector(img_src);
     }
 
     // 登録する画像のファイル名取得
@@ -11,6 +12,16 @@ export class Profile {
             const files = target.files;
             const file = files[0];
             this.newIconName.textContent = file.name;
+
+            // 画像のプレビュー表示
+            const prof = this;
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+           
+            reader.onload = ()=>{
+                prof.profileImage.src = reader.result;
+                // console.log(reader.result);   
+            };
         });
     }
 
