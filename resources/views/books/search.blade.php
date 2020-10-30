51dabd442bd13dd92096ff870b316822d4d7e3bb
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-10-30 10:53:38 +0900

    スタイルの修正_03（コンソールエラーの修正）

--- resources/views/books/search.blade.php
+++ resources/views/books/search.blade.php
@@ -5,20 +5,23 @@
 
 @section('content')
 <div class="search__container">
-  
+
     <!-- 検索フォーム -->
     @include('includes.rakuten_books.rakuten-book-search-form')
 
+    @if($items === null)
+    <div class="search__keyword">
+        <p>キーワード、またはISBNコードを入力してください。</p>
 
+    </div>
+
+    @else
     <div class="search__form-flex">
         <!-- 検索キーワード有無のチェック -->
         <div class="search__keyword">
-            @if($items === null)
-            <p>キーワード、またはISBNコードを入力してください。</p>
-            @else
             <p><span class="search__keyword-bold">{{ $keyword }}</span>の検索結果</p>
         </div>
-        
+
         <div>
             @if(Auth::check())
             <a href="{{route('books.index')}}">マイページに戻る</a>
@@ -39,4 +42,3 @@
     @endif
 </div>
 @endsection
-
