660db2aec8915aad67b913f1d8c7662f23ebd430
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-11-30 14:37:03 +0900

    定数の設定（画像処理:ステージング＆プロダクション）

--- config/consts/info.php
+++ config/consts/info.php
@@ -7,7 +7,11 @@ return [
             'url_staging' => 'https://book-review-shibucha.s3.ap-northeast-1.amazonaws.com/icons/',
             'url_production' => 'https://book-review-production.s3.ap-northeast-1.amazonaws.com/icons/',
         ],
-        'books' => [],
+        'books' => [
+            'url_local' => '/storage/books/',
+            'url_staging' => 'https://book-review-shibucha.s3.ap-northeast-1.amazonaws.com/books/',
+            'url_production' => 'https://book-review-production.s3.ap-northeast-1.amazonaws.com/books/',
+        ],
     ],
 
     'storage_folders' => [
@@ -15,6 +19,11 @@ return [
             'local' => 'public/icons',
             'staging' => 'icons',
             'production' => 'icons',
+        ],
+        'books' => [
+            'local' => 'public/books',
+            'staging' => 'books',
+            'production' => 'books',
         ],
     ],
 ];
