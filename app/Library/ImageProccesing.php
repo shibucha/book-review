d25263e88798c05d0ee555156a2fe222b39748f8
shibucha <ochachaaaaaaman@yahoo.co.jp>
2020-11-30 15:04:06 +0900

    変数名の見直し

--- app/Library/ImageProccesing.php
+++ app/Library/ImageProccesing.php
@@ -30,23 +30,23 @@ class ImageProccesing
         $image_env = EnvironmentalConfirmation::veryfyEnvironment();
 
         if ($image_env === "local") {
-            $icon_image_path_data = [
+            $icon_path = [
                 'icon_path' => config('consts.info.storage_folders.icons.local'),
                 'icon_url' => config('consts.info.aws_s3.icons.url_local'),
             ];
         } elseif ($image_env === "staging") {
-            $icon_image_path_data = [
+            $icon_path = [
                 'icon_path' => config('consts.info.storage_folders.icons.staging'),
                 'icon_url' => config('consts.info.aws_s3.icons.url_staging'),
             ];
         } else {
-            $icon_image_path_data = [
+            $icon_path = [
                 'icon_path' => config('consts.info.storage_folders.icons.production'),
                 'icon_url' => config('consts.info.aws_s3.icons.url_production'),
             ];
         }
 
-        return $icon_image_path_data;
+        return $icon_path;
     }
 
     public function getBookImagePath()
@@ -54,21 +54,21 @@ class ImageProccesing
         $image_env = EnvironmentalConfirmation::veryfyEnvironment();
 
         if ($image_env === "local") {
-            $book_image_path_data = [
+            $book_path = [
                 'book_path' => config('consts.info.storage_folders.books.local'),
                 'book_url' => config('consts.info.aws_s3.books.url_local'),
             ];
         } elseif ($image_env === "staging") {
-            $book_image_path_data = [
+            $book_path = [
                 'book_path' => config('consts.info.storage_folders.books.staging'),
                 'book_url' => config('consts.info.aws_s3.books.url_staging'),
             ];
         } else {
-            $book_image_path_data = [
+            $book_path = [
                 'book_path' => config('consts.info.storage_folders.books.production'),
                 'book_url' => config('consts.info.aws_s3.books.url_production'),
             ];
         }
-        return $book_image_path_data;
+        return $book_path;
     }
 }
