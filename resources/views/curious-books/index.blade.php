c36f4ab469d06d5f2b9dc598d3692757d668f9a2
shibucha <ochachaaaaaaman@yahoo.co.jp>
2021-01-27 17:48:14 +0900

    【読みたいリスト】スタイル修正（仮）

--- resources/views/curious-books/index.blade.php
+++ resources/views/curious-books/index.blade.php
@@ -9,22 +9,24 @@
     @include('includes.reading-history')
 
     <div class="curious__block">
-        <h1>読みたい本</h1>
+        <h1 class="curious__header-title">読みたい本</h1>
 
         @if(isset($curious_books))
         <div class="curious__lists">
             @foreach($curious_books as $book)
             <div class="curious__item">
-                <a href="{{route('books.show',['book_id'=>$book->book->book_id])}}">
-                    <img src="{{$book->book->image}}" alt="">
-                    <div class="curious__title">
-                        {{$book->book->title}}
-                        <form action="{{route('curious.update', ['book_isbn'=>$book->book->book_id])}}" method="post">
-                            @csrf
-                            <button type="submit">リストから外す</button>
-                        </form>
+                <a href="{{route('books.show',['book_id'=>$book->book->book_id])}}" class="curious__link">
+                    <div class="curious__img">
+                        <img src="{{$book->book->image}}" alt="">
                     </div>
                 </a>
+                <div class="curious__title">
+                    {{$book->book->title}}
+                </div>
+                <form class="curious__form" action="{{route('curious.update', ['book_isbn'=>$book->book->book_id])}}" method="post">
+                    @csrf
+                    <button type="submit" class="curious__form-btn">リストから外す</button>
+                </form>
             </div>
             @endforeach
         </div>
